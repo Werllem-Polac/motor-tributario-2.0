eee56a2e1d5766f6ff6722b6ddcc5af683b3a8af
Werllem-Polac <werllempolac@gmail.com>
2025-05-07 17:31:24 -0300

    procfile
    procfile

--- api/main.py
+++ api/main.py
@@ -128,4 +128,4 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Railway define PORT automaticamente
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
