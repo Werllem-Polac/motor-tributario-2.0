776104a90fe72fd1dd6996f9fd99ef3ccf06bca9
Werllem-Polac <werllempolac@gmail.com>
2025-05-10 19:09:37 -0300

    py
    1

--- api/main.py
+++ api/main.py
@@ -167,4 +167,4 @@ async def on_startup():
 # --- Execução local ---
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("api.main:app", host="0.0.0.0"
+    uvicorn.run("api.main:app", host="0.0.0.0")
